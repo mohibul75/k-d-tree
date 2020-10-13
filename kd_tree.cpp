f002addae65969d38c9fd39955490eea467ccc90
purbo75 <43554251+purbo75@users.noreply.github.com>
2020-10-13 13:43:54 +0600

    Add files via upload

---
+++ kd_tree.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<string.h>
+using namespace std;
+
+int k,depth=0;
+struct node {
+    int data[20];
+    node *right , *left ;
+};
+
+node *root=NULL;
+
+void insertNonRoot(node *curr,node *newData, int depth)
+{
+    int d=depth%k;
+
+    if(curr->right==NULL && curr->data[d] <= newData->data[d]) curr->right= newData;
+    else if(curr->left==NULL && curr->data[d] > newData->data[d]) curr->left= newData;
+    else if(curr->data[d] <= newData->data[d]) insertNonRoot(curr->right,newData,depth+1);
+    else insertNonRoot(curr->left,newData,depth+1);
+}
+
+void construct()
+{
+     struct node *temp=new node;
+//     temp->data=new int[k];
+    // temp = (node*)malloc( (sizeof( struct node) + (sizeof(int)*k)));
+    cout << "enter"<< "[" << k << "]  " << "points:    ";
+     for(int i=0 ;i<k ; i++)
+     {
+
+          cin >> temp->data[i];
+     }
+     temp->right=NULL;
+     temp->left=NULL;
+
+
+    if(root==NULL) root=temp;
+    else insertNonRoot(root,temp,0);
+
+}
+
+void print(node *newNode)
+{
+    if(newNode==NULL) return;
+
+    print(newNode->left);
+
+    for(int i=0 ; i<k ; i++)
+    {
+        cout<<newNode->data[i]<<"     ";
+    }
+    cout << endl;
+
+    print(newNode->right);
+}
+
+bool sameNode(int *arr, int *brr)
+{
+    for (int i = 0; i < k; ++i)
+    {
+            if (arr[i] != brr[i])
+            {
+                  return false;
+            }
+    }
+
+    return true;
+}
+
+bool search_node(node* curr, int *point, int hight)
+{
+    if (curr == NULL)
+        return false;
+    if (sameNode(curr->data, point))
+    {
+        depth=hight;
+         return true;
+    }
+
+
+    int di = hight % k;
+
+    if (point[di] < curr->data[di])
+        return search_node(curr->left, point, hight + 1);
+
+    return search_node(curr->right, point, hight + 1);
+}
+
+void search_fun()
+{
+    int s_point[k];
+    cout << "enter a node  :  ";
+    for(int i=0 ; i<k ; i++) cin >> s_point[i];
+    bool bl=search_node(root,s_point,0);
+    if(bl==true) cout <<"found and hight :  " << depth+1 << endl;
+    else cout <<"Not found " << endl;
+}
+
+int main(void)
+{
+     cout << "enter the value of K : " ;
+    cin >> k;
+
+   // root =(node*)malloc( (sizeof( int)*(k+2) ));
+
+    // int *arr;
+    //arr =  new int [4];
+    // root=(node*)malloc( (sizeof( root) + (sizeof(int)*k)));;
+
+  //  cout << sizeof(root) <<endl;
+   // cout << sizeof(root->data)<<endl;
+   // cout << sizeof(root->left)<<endl;
+   // cout << sizeof(root->right)<<endl;
+  //  cout << sizeof(arr)<<endl;
+
+   while(true)
+   {
+        int choice;
+
+        cout<<"1.inset\n2.Search\n3.print(in-order traversal)\n4.exit "<<endl;
+        cout<<"enter your choice : "<<endl;
+        cin>>choice;
+
+        switch(choice)
+        {
+
+            case 1:
+                construct();
+                break;
+
+            case 2:
+                search_fun();
+                break;
+
+            case 3:
+                print(root);
+                break;
+
+        }
+
+        if(choice==4) break;
+
+   }
+    return 0;
+}
